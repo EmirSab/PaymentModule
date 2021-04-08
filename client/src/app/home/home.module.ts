f7370ceca798e3caf8602a3cae62aa312d5b05ac
Emir Šabanić <emir.sabanic.ro@gmail.com>
2021-04-08 10:50:40 +0200

    Changing some html and fixing errors

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 // 10.108.1 Export home component -> app.module.ts
+// 12.132.1 Add shared module -> home.html
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule, SharedModule
   ],
   exports: [HomeComponent]
 })
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 
@@ -8,13 +9,14 @@ import { PagerComponent } from './components/pager/pager.component';
 //9.101.3 Eksportovati paging header ->shop.html
 // 9.102 Add new component pager, addit to the shared module, turn paging html in shared component
 // and export pager component ->
+//12.132 CarouselModule -> home.module.ts
 
 @NgModule({
   declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(), CarouselModule.forRoot(),
   ],
-  exports: [PaginationModule, PagingHeaderComponent, PagerComponent]
+  exports: [PaginationModule, PagingHeaderComponent, PagerComponent, CarouselModule]
 })
 export class SharedModule { }
