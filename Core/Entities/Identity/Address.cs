7b3f66765eb8017c261d7991c39683d2b3449549
Emir Šabanić <emir.sabanic.ro@gmail.com>
2021-04-30 16:54:07 +0200

    Adding Identity classes and properties

---
+++ Core/Entities/Identity/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Identity
+{
+    public class Address
+    {
+        // 15.162.1 Add properties for address ->
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
---
+++ Core/Entities/Identity/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        //15.162 Add AppUser class for user properties  -> Address.cs
+        public string DisplayName { get; set; }
+        public Address Address { get; set; }
+    }
+}
